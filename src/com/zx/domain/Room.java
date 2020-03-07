de8edfebc754f6712d4550d3f1b2f4ddf0e75102
ZexiGong <gong.ze@husky.neu.edu>
2020-03-07 15:58:21 +0800

    mybatis code helper

---
+++ src/com/zx/domain/Room.java
@@ -0,0 +1,53 @@
+package com.zx.domain;
+
+public class Room {
+    private Integer id;
+
+    private Integer type;
+
+    private Integer price;
+
+    private String info;
+
+    private Integer hotelid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Integer getHotelid() {
+        return hotelid;
+    }
+
+    public void setHotelid(Integer hotelid) {
+        this.hotelid = hotelid;
+    }
+}
